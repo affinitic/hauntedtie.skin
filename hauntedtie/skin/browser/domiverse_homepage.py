2581260c1f091d2656edc30361822c86b4535ae5
schminitz <gillian@affinitic.be>
2015-05-19 10:44:02 +0200

    Add custom view for front page

---
+++ hauntedtie/skin/browser/domiverse_homepage.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+from Products.Five.browser import BrowserView
+
+
+class DomiverseHomepage(BrowserView):
+    """
+    """
---
+++ hauntedtie/skin/setuphandlers.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from Products.CMFCore.utils import getToolByName
+
+
+def setupHauntedTie(context):
+    if context.readDataFile('hauntedtie.portal_various.txt') is None:
+        return
+    portal = context.getSite()
+    front_page = portal.get('front-page')
+
+    changeFolderView(portal, front_page, 'domiverse_homepage_view')
+
+
+def changeFolderView(portal, document, viewname):
+    addViewToType(portal, 'Document', viewname)
+    if document.getLayout() != viewname:
+        document.setLayout(viewname)
+
+
+def addViewToType(portal, typename, templatename):
+    pt = getToolByName(portal, 'portal_types')
+    foldertype = getattr(pt, typename)
+    available_views = list(foldertype.getAvailableViewMethods(portal))
+    if templatename not in available_views:
+        available_views.append(templatename)
+        foldertype.manage_changeProperties(view_methods=available_views)
