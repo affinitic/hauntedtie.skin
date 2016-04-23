43efa48169c9bed9f60b5bf00c49b8425ccc5d30
schminitz <gillian@affinitic.be>
2016-04-23 13:43:37 +0200

    Begin new skin of domiverse website

--- hauntedtie/skin/static/domiverse/js/domiverse.js
+++ hauntedtie/skin/static/domiverse/js/domiverse.js
@@ -15,4 +15,34 @@ jQuery(document).ready(function ($) {
         .fancybox({
             padding : 0
     });
+
+    // Add parallax to banner
+    $("#banner-0").parallax("100%", -0.3);
+    $("#banner-1").parallax("100%", -0.15);
+    $("#banner-2").parallax("100%", 0);
+
+    // Menu scroll to anchor
+    function scrollToAnchor(selector){
+        var aTag = $(selector);
+        $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+    }
+
+    $("#nav a").click(function(e) {
+        e.preventDefault();
+        href = $(this).attr('href');
+        scrollToAnchor(href);
+        return false;
+    });
+
+    // Fix menu to top
+    $(window).scroll(function(e){
+      var $el = $('#section-menu');
+      if ($(this).scrollTop() > 720){
+        $el.css({'position': 'fixed', 'top': '0px'});
+      }
+      if ($(this).scrollTop() < 720)
+      {
+        $el.css({'position': 'absolute', 'top': '720px'});
+      }
+    });
 });
